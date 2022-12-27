6b0e822c582bcc35fcc6ca917c1dca9d5fabfc23
ejdam87 <adam.dzadon@gmail.com>
2022-12-28 00:08:18 +0100

    Refactor vector + change classification return value

--- perceptron.py
+++ perceptron.py
@@ -1,47 +1,22 @@
 import random
+from vector import Vector
 
-Vector = list[ float ]
-
-def dot( v1: Vector, v2: Vector ) -> float:
-
-    res = 0
-    for a, b in zip( v1, v2 ):
-        res += a * b
-    return res
-
-def plus( v1: Vector, v2: Vector ) -> Vector:
-    res = []
-    for a, b in zip( v1, v2 ):
-        res.append( a + b )
-    return res
-
-def minus( v1: Vector, v2: Vector ) -> Vector:
-    res = []
-    for a, b in zip( v1, v2 ):
-        res.append( a - b )
-    return res
-
-def mult( v: Vector, c: int ) -> Vector:
-    return [ c * a for a in v ]
-
-def extended( v: Vector ) -> Vector:
-    return [ 1 ] + v
 
 class Perceptron:
 
     def __init__( self, dim: int ) -> None:
         self.dim = dim
-        self.weights = [ random.random( ) for _ in range( dim + 1 ) ]
+        self.weights = Vector( [ random.random( ) for _ in range( dim + 1 ) ] )
 
     def set_weights( self, v: Vector ) -> None:
         self.weights = v
 
-    def classify( self, vector: Vector ) -> bool:
-        return self.weights[ 0 ] + dot( self.weights[ 1: ], vector ) >= 0
+    def classify( self, vector: Vector ) -> int:
+        return 1 if self.weights * vector >= 0 else 0
 
 
-train = [ ( [ -1, 0 ], True ), ( [ 0, 1 ], True ), ( [ 3, 0 ], False ) ]
-Sample = tuple[ Vector, bool ]
+train = [ ( Vector( [ -1, 0 ] ), 1 ), ( Vector( [ 0, 1 ] ), 1 ), ( Vector( [ 3, 0 ] ), 0 ) ]
+Sample = tuple[ Vector, int ]
 
 def learn( p: Perceptron, epochs: int, dataset: list[ Sample ] ) -> None:
     
@@ -49,8 +24,14 @@ def learn( p: Perceptron, epochs: int, dataset: list[ Sample ] ) -> None:
     for i in range( epochs ):
 
         sample, exp = dataset[ i % n ]
+        sample = sample.extended( 1 )
+
         actual = p.classify( sample )
 
-        diff = int( actual ) - int( exp )
-        new = minus( p.weights, mult( extended( sample ), diff ) )
-        p.weights = new
+        diff = actual - exp
+        p.weights = p.weights - diff * sample
+
+
+p = Perceptron( 2 )
+p.set_weights( Vector( [0, 1, -1] ) )
+learn( p, 6, train )
